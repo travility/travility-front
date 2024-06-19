29b432b92b87251e3db6e9100bfd80b4fdbe8322
heo5620 <tnqls1035@naver.com>
2024-06-19 22:42:32 +0900

    [css] 캘린더 css 수정

--- src/pages/dashboard/MyCalendar.js
+++ src/pages/dashboard/MyCalendar.js
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
-import moment from "moment";
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import moment from 'moment';
 import DefaultSidebar from '../../components/DefaultSidebar';
-import styles from '../../styles/dashboard/MyCalendar.module.css';
+import styles from '../../styles/dashboard/Calendar.css';
 
 const MyCalendar = () => {
   const today = new Date();
   const [date, setDate] = useState(today);
   const [activeStartDate, setActiveStartDate] = useState(new Date());
-  const journeyDay = ["2024-06-03", "2024-06-13"]; //여행날짜
+  const journeyDay = ['2024-06-03', '2024-06-13']; // 여행 날짜
 
   const handleDateChange = (newDate) => {
     setDate(newDate);
@@ -21,61 +20,60 @@ const MyCalendar = () => {
     setDate(today);
   };
 
-
   return (
-    <div className={styles.mycalendar_page}>
+    <div className={styles.mycalendar_container}>
       <DefaultSidebar />
       <div className={styles.mycalendar_container}>
-      <Calendar
-        className={styles.react_calendar}
-        value={date}
-        onChange={handleDateChange}
-        formatDay={(locale, date) => moment(date).format("D")}
-        formatYear={(locale, date) => moment(date).format("YYYY")}
-        formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")}
-        calendarType="gregory"
-        showNeighboringMonth={false}
-        next2Label={null}
-        prev2Label={null}
-        minDetail="year"
-        activeStartDate={activeStartDate}
-        onActiveStartDateChange={({ activeStartDate }) =>
-          setActiveStartDate(activeStartDate)
-        }
-
-        tileContent={({ date, view }) => {
-          const elements = [];
-          if (
-            view === "month" &&
-            date.getMonth() === today.getMonth() &&
-            date.getDate() === today.getDate()
-          ) {
-            elements.push(
-              <div key={"today"} 
-              className={styles.mycalendar_today}
-              >
-                오늘
-              </div>
-            );
+        <Calendar
+          value={date}
+          onChange={handleDateChange}
+          formatDay={(locale, date) => moment(date).format('D')}
+          formatYear={(locale, date) => moment(date).format('YYYY')}
+          formatMonthYear={(locale, date) => moment(date).format('YYYY. MM')}
+          calendarType="gregory"
+          showNeighboringMonth={false}
+          next2Label={null}
+          prev2Label={null}
+          minDetail="year"
+          activeStartDate={activeStartDate}
+          onActiveStartDateChange={({ activeStartDate }) =>
+            setActiveStartDate(activeStartDate)
           }
-          if (journeyDay.find((x) => x === moment(date).format("YYYY-MM-DD"))) {
-            elements.push(
-              <div
-                key={moment(date).format("YYYY-MM-DD")}
-                className={styles.mycalendar_today_dot}
-              />
-            );
-          }
-          return <>{elements}</>;
-        }}
-      />
-      <div className={styles.myclaendar_click_today} onClick={handleTodayClick}>
-        오늘
+          tileContent={({ date, view }) => {
+            const elements = [];
+            if (
+              view === 'month' &&
+              date.getMonth() === today.getMonth() &&
+              date.getDate() === today.getDate()
+            ) {
+              elements.push(
+                <div key={'today'} className={styles.mycalendar_today}>
+                  오늘
+                </div>
+              );
+            }
+            if (
+              journeyDay.find((x) => x === moment(date).format('YYYY-MM-DD'))
+            ) {
+              elements.push(
+                <div
+                  key={moment(date).format('YYYY-MM-DD')}
+                  className={styles.mycalendar_today_dot}
+                />
+              );
+            }
+            return <>{elements}</>;
+          }}
+        />
+        <div
+          className={styles.mycalendar_click_today}
+          onClick={handleTodayClick}
+        >
+          오늘
+        </div>
       </div>
     </div>
-    </div>
   );
-
 };
 
 export default MyCalendar;
