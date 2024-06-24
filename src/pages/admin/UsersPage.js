97712826138c0c9e2c45214a4d82556c5ddfbe07
heo5620 <tnqls1035@naver.com>
2024-06-24 10:01:55 +0900

    [feat] 사용자 관리 페이지

--- src/pages/admin/UsersPage.js
+++ src/pages/admin/UsersPage.js
@@ -3,6 +3,7 @@ import { getMemberList } from '../../api/adminApi';
 import { handleAccessDenied } from '../../util/logoutUtils';
 import { useNavigate } from 'react-router-dom';
 import DefaultSidebar from '../../components/DefaultSidebar';
+import styles from '../../styles/admin/UserPage.module.css';
 
 const UsersPage = () => {
   const [memberList, setMemberList] = useState([]);
@@ -23,12 +24,35 @@ const UsersPage = () => {
   }, [navigate]);
 
   return (
-    <div>
+    <div className={styles.usersPage}>
       <DefaultSidebar />
-      <div>
-        {memberList.map((member) => {
-          return <div key={member.username}>{member.username}</div>;
-        })}
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <img
+            src="/images/admin/statistics.png"
+            alt="statics"
+            className={styles.image}
+          ></img>
+          <span className={styles.title}>사용자 관리</span>
+        </div>
+        <div className={styles.statistics}>
+          <p>
+            총 회원 수는 <span>?</span> 명입니다.
+          </p>
+          <p>
+            오늘 신규 가입자 수는 <span>?</span> 명입니다.
+          </p>
+        </div>
+        <div className={styles.memberList}>
+          {memberList.map((member) => {
+            return (
+              <div key={member.username} className={styles.member}>
+                <span>{member.username}</span>
+                <span>{member.email}</span>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
