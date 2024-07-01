876d3ba0ad87098f41249a20e846c58a9a71f4c1
heo5620 <tnqls1035@naver.com>
2024-07-01 16:11:37 +0900

    [fix] 마이페이지, 관리자 페이지, header 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,37 @@
-import React, { useContext, useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import styles from "../styles/components/Header.module.css";
-import { logout } from "../api/memberApi";
+import React, { useContext, useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import styles from '../styles/components/Header.module.css';
+import { logout } from '../api/memberApi';
 import {
   handleAlreadyLoggedOut,
   handleSuccessLogout,
   handleTokenExpirationLogout,
-} from "../util/logoutUtils";
-import { TokenStateContext } from "../App";
+} from '../util/logoutUtils';
+import { TokenStateContext } from '../App';
 
 const Header = () => {
   const { tokenStatus, memberInfo } = useContext(TokenStateContext);
-  const [username, setUsername] = useState("");
-  const [role, setRole] = useState("");
+  const [name, setName] = useState('');
+  const [role, setRole] = useState('');
   const navigate = useNavigate();
   const location = useLocation();
 
   useEffect(() => {
     if (memberInfo) {
-      setUsername(memberInfo.username || "");
-      setRole(memberInfo.role || "");
+      setName(memberInfo.name || '');
+      setRole(memberInfo.role || '');
     }
   }, [memberInfo]);
 
   const handleLogout = async () => {
     try {
-      if (tokenStatus === "Token valid") {
+      if (tokenStatus === 'Token valid') {
         await logout();
         handleSuccessLogout(navigate);
-      } else if (tokenStatus === "Token expired") {
+      } else if (tokenStatus === 'Token expired') {
         await logout();
         handleTokenExpirationLogout(navigate);
-      } else if (tokenStatus === "Token null") {
+      } else if (tokenStatus === 'Token null') {
         handleAlreadyLoggedOut(navigate);
       }
     } catch (error) {
@@ -40,15 +40,15 @@ const Header = () => {
   };
 
   const handleLogoClick = () => {
-    if (tokenStatus === "Token valid") {
-      navigate("/main");
+    if (tokenStatus === 'Token valid') {
+      navigate('/main');
     } else {
-      navigate("/");
+      navigate('/');
     }
   };
 
   const logoStyle = {
-    color: location.pathname === "/" ? "#fff" : "var(--main-color)",
+    color: location.pathname === '/' ? '#fff' : 'var(--main-color)',
   };
 
   return (
@@ -60,17 +60,17 @@ const Header = () => {
       >
         Travility
       </div>
-      {(tokenStatus === "Token valid" || location.pathname !== "/") && (
+      {(tokenStatus === 'Token valid' || location.pathname !== '/') && (
         <div className={styles.header_user_container}>
-          {tokenStatus === "Token valid" && (
+          {tokenStatus === 'Token valid' && (
             <>
               <span className={styles.header_welcome_message}>
-                {role === "ROLE_ADMIN" ? (
+                {role === 'ROLE_ADMIN' ? (
                   <>현재 관리자 모드입니다</>
                 ) : (
                   <>
                     <img src="/images/person_circle.png" alt="user" />
-                    {username} 님 반갑습니다!
+                    {name} 님 반갑습니다!
                   </>
                 )}
               </span>
@@ -78,17 +78,19 @@ const Header = () => {
                 <button className={styles.logout_button} onClick={handleLogout}>
                   Logout
                 </button>
-                {role === "ROLE_ADMIN" ? (
+                {role === 'ROLE_ADMIN' ? (
                   <button
                     className={styles.nav_second_button}
-                    onClick={() => navigate("/admin/users")}
+                    onClick={() => navigate('/admin/users')}
                   >
                     관리
                   </button>
+                ) : location.pathname === '/' ? (
+                  <></>
                 ) : (
                   <button
                     className={styles.nav_second_button}
-                    onClick={() => navigate("/")}
+                    onClick={() => navigate('/')}
                   >
                     About Us
                   </button>
