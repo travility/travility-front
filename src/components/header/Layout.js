031999a3fd50cd30188b0d2a13844693a7829cf4
choisojin <sojn6093@gmail.com>
2024-07-17 17:38:21 +0900

    [fix] 반응형 추가

--- src/components/header/Layout.js
+++ src/components/header/Layout.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import Header from './Header';
-import NavigationBar from '../../components/header/NavigationBar';
-import { Inner, Container } from '../../styles/StyledComponents';
-import { Outlet } from 'react-router-dom';
-import ScrollToTopButton from '../ScrollToTopButton';
+import React from "react";
+import Header from "./Header";
+import NavigationBar from "../../components/header/NavigationBar";
+import { Inner, Container } from "../../styles/StyledComponents";
+import { Outlet } from "react-router-dom";
+import ScrollToTopButton from "../ScrollToTopButton";
 
 const Layout = () => {
   return (
     <div>
       <Inner>
-        <Header />
+        <div className="header-wrapper">
+          <Header />
+        </div>
         <NavigationBar />
         <Container>
           <Outlet />
---
+++ src/pages/accountbook/detail/AccountBookMenu.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button } from "../../../styles/StyledComponents";
+import styles from "../../../styles/accountbook/AccountBookDetail.module.css";
+
+const AccountBookMenu = ({ onBudgetClick, onExpenseClick }) => {
+  return (
+    <div className={styles.accountbook_icons}>
+      <span>
+        <Button>
+          <img src="/images/account/statistic.png" alt="statistic" />
+          <p>지출 통계</p>
+        </Button>
+      </span>
+      <span>
+        <Button onClick={onBudgetClick}>
+          <img src="/images/account/local_atm.png" alt="budget" />
+          <p>예산 추가</p>
+        </Button>
+      </span>
+      <span>
+        <Button onClick={onExpenseClick}>
+          <img src="/images/account/write.png" alt="addExpense" />
+          <p>지출 추가</p>
+        </Button>
+      </span>
+    </div>
+  );
+};
+
+export default AccountBookMenu;
