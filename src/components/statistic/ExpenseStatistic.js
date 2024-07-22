af7fbe1aae5357459810f409a3dd30cd57f2c49c
문태준 <pjrnoon99@gmail.com>
2024-07-22 20:36:44 +0900

    [fix] 변경 없음

--- src/components/statistic/ExpenseStatistic.js
+++ src/components/statistic/ExpenseStatistic.js
@@ -2,93 +2,73 @@ import React, { useEffect, useState } from 'react';
 import { Bar, Line } from 'react-chartjs-2';
 import Select from 'react-select';
 import { Tooltip } from 'react-tooltip';
-import {
-  getExpenseStatisticsByDate,
-  getPaymentMethodStatisticsByDate,
-  getStatisticsByCategoryAndDates,
-} from '../../api/expenseApi';
+import { getExpenseStatisticsByDate, getPaymentMethodStatisticsByDate, getStatisticsByCategoryAndDates } from "../../api/expenseApi";
 import 'chart.js/auto';
 import TotalAmountCategory from './TotalAmountCategory';
 import TotalResult from '../statistic/TotalResult';
 import styles from '../../styles/statistic/ExpenseStatistic.module.css';
 import { formatDate } from '../../util/calcUtils';
-import { Button } from '../../styles/StyledComponents';
-import { useNavigate, useParams } from 'react-router-dom';
 
 // 카테고리 목록
-
 const categories = [
-  { en: 'ACCOMMODATION', ko: '숙박' },
   { en: 'TRANSPORTATION', ko: '교통' },
-  { en: 'SHOPPING', ko: '쇼핑' },
+  { en: 'ACCOMMODATION', ko: '숙박' },
   { en: 'FOOD', ko: '식비' },
   { en: 'TOURISM', ko: '관광' },
+  { en: 'SHOPPING', ko: '쇼핑' },
   { en: 'OTHERS', ko: '기타' },
-  { en: 'ALL', ko: '전체지출' },
+  { en: 'ALL', ko: '전체지출' }
 ];
 
 const categoryMap = {
-  ACCOMMODATION: '숙박',
-  TRANSPORTATION: '교통',
-  SHOPPING: '쇼핑',
-  FOOD: '식비',
-  TOURISM: '관광',
-  OTHERS: '기타',
-  ALL: '전체지출',
+  'TRANSPORTATION': '교통',
+  'ACCOMMODATION': '숙박',
+  'FOOD': '식비',
+  'TOURISM': '관광',
+  'SHOPPING': '쇼핑',
+  'OTHERS': '기타',
+  'ALL': '전체지출'
 };
 
 // 카테고리 색 배열
-const colors = [
-  '#23C288',
-  '#7697F9',
-  '#9B80E9',
-  '#FEC144',
-  '#B5CE2A',
-  '#828C98',
-];
+const colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#a9a9a9'];
 
 // 결제 방법 목록
 const paymentMethods = [
-  { en: 'CASH', ko: '현금' },
   { en: 'CARD', ko: '카드' },
+  { en: 'CASH', ko: '현금' }
 ];
 
 // 결제방법 색 배열
-const paymentColors = ['#FFBBE5', '#2c73d2'];
+const paymentColors = ['#A39E93', '#4c66f8'];
 
-const ExpenseStatistic = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
+const ExpenseStatistic = ({ accountBookId, onBack }) => {
   const [statistics, setStatistics] = useState([]);
   const [paymentMethodStatistics, setPaymentMethodStatistics] = useState([]);
   const [selectedDate, setSelectedDate] = useState('');
   const [dates, setDates] = useState([]);
-  const [selectedCategories, setSelectedCategories] = useState(['ALL']);
-  const [lineChartData, setLineChartData] = useState({
-    labels: [],
-    datasets: [],
-  });
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [lineChartData, setLineChartData] = useState({ labels: [], datasets: [] });
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await getExpenseStatisticsByDate(id);
-        console.log(data);
+        const data = await getExpenseStatisticsByDate(accountBookId);
         setStatistics(data);
 
-        const uniqueDates = Array.from(new Set(data.map((item) => item.date)));
+        const uniqueDates = Array.from(new Set(data.map(item => item.date)));
         setDates(uniqueDates);
-        setSelectedDate(uniqueDates[0]);
+        setSelectedDate(uniqueDates[0]); 
 
         const paymentData = await getPaymentMethodStatisticsByDate(accountBookId, uniqueDates[0]);
         if (Array.isArray(paymentData)) {
           setPaymentMethodStatistics(paymentData);
         } else {
-          console.error('Expected an array but got:', paymentData);
+          console.error("Expected an array but got:", paymentData);
           setPaymentMethodStatistics([]);
         }
       } catch (error) {
-        console.error('Failed to fetch statistics:', error);
+        console.error("Failed to fetch statistics:", error);
       }
     };
 
@@ -97,23 +77,20 @@ const ExpenseStatistic = () => {
 
   const handleDateChange = async (selectedOption) => {
     setSelectedDate(selectedOption.value);
-    const paymentData = await getPaymentMethodStatisticsByDate(
-      id,
-      selectedOption.value
-    );
+    const paymentData = await getPaymentMethodStatisticsByDate(accountBookId, selectedOption.value);
     if (Array.isArray(paymentData)) {
       setPaymentMethodStatistics(paymentData);
     } else {
-      console.error('Expected an array but got:', paymentData);
+      console.error("Expected an array but got:", paymentData);
       setPaymentMethodStatistics([]);
     }
   };
 
   const handleCategoryChange = async (event) => {
     const value = event.target.value;
-    setSelectedCategories((prevSelectedCategories) =>
-      prevSelectedCategories.includes(value)
-        ? prevSelectedCategories.filter((category) => category !== value)
+    setSelectedCategories(prevSelectedCategories => 
+      prevSelectedCategories.includes(value) 
+        ? prevSelectedCategories.filter(category => category !== value) 
         : [...prevSelectedCategories, value]
     );
   };
@@ -130,40 +107,31 @@ const ExpenseStatistic = () => {
     const fetchLineChartData = async () => {
       if (selectedCategories.length > 0) {
         try {
-          const datasets = await Promise.all(
-            selectedCategories.map(async (category) => {
-              const data = await getStatisticsByCategoryAndDates(id, category);
-              const colorIndex = categories.findIndex(
-                (cat) => cat.en === category
-              );
-              const backgroundColor = colors[colorIndex] + '80'; // 살짝 투명하게함 50%
-              return {
-                label: categoryMap[category], // 한글 라벨
-                data: dates.map((date) => {
-                  const entry = data.find((d) => d.date === date);
-                  return entry ? entry.amount : 0;
-                }),
-                borderColor: colors[colorIndex],
-                backgroundColor: backgroundColor,
-                pointStyle: 'circle', // 포인트 스타일 (꼭짓점)
-                pointRadius: 10, // 포인트 크기
-                pointHoverRadius: 15, // 호버 포인트 크기 설정
-                pointBackgroundColor: backgroundColor,
-                pointBorderColor: colors[colorIndex], // 포인트 테두리 색상
-              };
-            })
-          );
+          const datasets = await Promise.all(selectedCategories.map(async (category) => {
+            const data = await getStatisticsByCategoryAndDates(accountBookId, category);
+            const colorIndex = categories.findIndex(cat => cat.en === category);
+            const backgroundColor = colors[colorIndex] + '80'; // 살짝 투명하게함 50%
+            return {
+              label: categoryMap[category], // 한글 라벨
+              data: dates.map(date => {
+                const entry = data.find(d => d.date === date);
+                return entry ? entry.amount : 0;
+              }),
+              borderColor: colors[colorIndex],
+              backgroundColor: backgroundColor,
+              pointStyle: 'circle', // 포인트 스타일 (꼭짓점)
+              pointRadius: 10, // 포인트 크기
+              pointHoverRadius: 15, // 호버 포인트 크기 설정
+              pointBackgroundColor: backgroundColor,
+              pointBorderColor: colors[colorIndex] // 포인트 테두리 색상
+            };
+          }));
           setLineChartData({ labels: dates.map(formatDate), datasets });
         } catch (error) {
-          console.error('Failed to fetch line chart data:', error);
+          console.error("Failed to fetch line chart data:", error);
         }
       } else {
-        setLineChartData({
-          labels: dates.map(formatDate),
-          datasets: [
-            { label: '', data: [], borderColor: 'rgba(0,0,0,0)', fill: false },
-          ],
-        });
+        setLineChartData({ labels: dates.map(formatDate), datasets: [{ label: '', data: [], borderColor: 'rgba(0,0,0,0)', fill: false }] });
       }
     };
 
@@ -173,37 +141,31 @@ const ExpenseStatistic = () => {
   }, [selectedCategories, dates]);
 
   const filteredData = statistics
-    .filter((stat) => stat.date === selectedDate)
+    .filter(stat => stat.date === selectedDate)
     .reduce((acc, stat) => {
       acc[stat.category] = stat.amount;
       return acc;
     }, {});
 
   const data = {
-    labels: categories.map((category) => category.ko),
-    datasets: [
-      {
-        label: '금액',
-        data: categories.map((category) => filteredData[category.en] || 0),
-        backgroundColor: colors,
-      },
-    ],
+    labels: categories.map(category => category.ko),
+    datasets: [{
+      label: '금액',
+      data: categories.map(category => filteredData[category.en] || 0),
+      backgroundColor: colors,
+    }]
   };
 
   const paymentMethodData = {
-    labels: paymentMethods.map((method) => method.ko),
-    datasets: [
-      {
-        label: '금액',
-        data: paymentMethods.map((method) => {
-          const stat = paymentMethodStatistics.find(
-            (stat) => stat.paymentMethod === method.en
-          );
-          return stat ? stat.amount : 0;
-        }),
-        backgroundColor: paymentColors,
-      },
-    ],
+    labels: paymentMethods.map(method => method.ko),
+    datasets: [{
+      label: '금액',
+      data: paymentMethods.map(method => {
+        const stat = paymentMethodStatistics.find(stat => stat.paymentMethod === method.en);
+        return stat ? stat.amount : 0;
+      }),
+      backgroundColor: paymentColors,
+    }]
   };
 
   const options = {
@@ -211,18 +173,12 @@ const ExpenseStatistic = () => {
       x: {
         beginAtZero: true,
         grid: {
-          lineWidth: 1, // x축 모든 라인 굵게
-          color: '#d1d1d1',
           lineWidth: 1,
           color: "#d1d1d1"
         },
         ticks: {
-          color: '#000000', // 폰트 색상
           color: "#000000",
           font: {
-            size: 15, // 폰트 크기
-          },
-        },
             size: 15
           }
         }
@@ -230,13 +186,10 @@ const ExpenseStatistic = () => {
       y: {
         beginAtZero: true,
         grid: {
-          lineWidth: 1, // y축의 모든 라인 굵게
-          color: '#d1d1d1',
           lineWidth: 1,
           color: "#d1d1d1"
         },
         ticks: {
-          color: '#000000', // 폰트 색상
           color: "#000000",
           font: {
             size: 14
@@ -261,11 +214,11 @@ const ExpenseStatistic = () => {
           label: (context) => `${context.dataset.label}: ${context.raw}`,
           labelColor: (context) => ({
             borderColor: context.dataset.borderColor,
-            backgroundColor: context.dataset.backgroundColor,
-          }),
-        },
-      },
-    },
+            backgroundColor: context.dataset.backgroundColor
+          })
+        }
+      }
+    }
   };
 
   const categoryChartOptions = {
@@ -305,10 +258,8 @@ const ExpenseStatistic = () => {
             return dataset.data.map((data, index) => ({
               text: `${chart.data.labels[index]}`,
               fillStyle: dataset.backgroundColor[index],
-              hidden:
-                dataset._meta &&
-                dataset._meta[Object.keys(dataset._meta)[0]].data[index].hidden,
-              index: index,
+              hidden: dataset._meta && dataset._meta[Object.keys(dataset._meta)[0]].data[index].hidden,
+              index: index
             }));
           },
           color: "black",
@@ -324,20 +275,8 @@ const ExpenseStatistic = () => {
 
           meta.data[index].hidden = !meta.data[index].hidden;
           ci.update();
-        },
+        }
       },
-      datasets: [
-        {
-          backgroundColor: [
-            '#23C288',
-            '#7697F9',
-            '#9B80E9',
-            '#FEC144',
-            '#B5CE2A',
-            '#828C98',
-          ],
-        },
-      ],
       tooltip: {
         mode: 'index',
         intersect: false,
@@ -345,11 +284,11 @@ const ExpenseStatistic = () => {
           label: (context) => `${context.dataset.label}: ${context.raw}`,
           labelColor: (context) => ({
             borderColor: context.dataset.borderColor,
-            backgroundColor: context.dataset.backgroundColor,
-          }),
-        },
-      },
-    },
+            backgroundColor: context.dataset.backgroundColor
+          })
+        }
+      }
+    }
   };
 
   const paymentMethodOptions = {
@@ -390,7 +329,7 @@ const ExpenseStatistic = () => {
               text: `${chart.data.labels[index]}`,
               fillStyle: dataset.backgroundColor[index],
               hidden: false,
-              index: index,
+              index: index
             }));
           },
           color: "black",
@@ -407,34 +346,26 @@ const ExpenseStatistic = () => {
           label: (context) => `${context.dataset.label}: ${context.raw}`,
           labelColor: (context) => ({
             borderColor: context.dataset.borderColor,
-            backgroundColor: context.dataset.backgroundColor,
-          }),
-        },
-      },
-    },
+            backgroundColor: context.dataset.backgroundColor
+          })
+        }
+      }
+    }
   };
 
   const dateOptions = dates.map(date => ({ value: date, label: formatDate(date) }));
 
   return (
-    <div className={styles.expenseStatistic}>
-      <div className={styles.expenseStatistic_header}>
-        <div className={styles.expenseStatistic_buttonContainer}>
-          <Button
-            onClick={goBack}
-            className={styles.expenseStatistic_backButton}
-          >
-            ←
-          </Button>
-        </div>
-        <div className={styles.expenseStatistic_budgetContainer}>
-          <TotalResult accountBookId={id} />
-        </div>
+    <div className={styles.statisticsContainer}>
+      <div className={styles.headerContainer}>
+        <button onClick={onBack} className={styles.backButton}>뒤로가기</button>
+        
+        <TotalResult accountBookId={accountBookId} />
+        
       </div>
-      <div className={styles.expenseStatistic_content}>
-        {/* <h2 className={styles.expenseStatistic_totalAmountHeader}>총 지출 내역</h2> */}
-        {/* <a
-          className={styles.expenseStatistic_tooltipLink}
+      <h2 className={styles.totalAmountHeader}>총 지출 내역</h2>
+      <a
+          className={styles.tooltipLink}
           data-tooltip-id="expense-info"
           data-tooltip-content="개인 지출 + (공유 지출 / 인원수)"
           data-tooltip-variant="info"
@@ -470,14 +401,23 @@ const ExpenseStatistic = () => {
         <div className={styles.lineChartWrapper}>
           <Line data={lineChartData} options={options} /> {/* Line = 라인차트 */}
         </div>
-        <div className={styles.expenseStatistic_lineChartContainer}>
-          <div className={styles.expenseStatistic_lineChartWrapper}>
-            <Line
-              className={styles.lineChart}
-              data={lineChartData}
-              options={options}
-            />
-          </div>
+      </div>
+      <div className={styles.selectContainer}>
+        <Select
+          options={dateOptions}
+          onChange={handleDateChange}
+          placeholder="날짜 선택"
+          noOptionsMessage={() => "선택 가능한 날짜가 없습니다."}
+        />
+      </div>
+      <div className={styles.chartsWrapper}>
+        <div className={`${styles.chartContainer} ${styles.expenseChartContainer}`}>
+          <h3 className={styles.chartTitle}>항목별 지출</h3>
+          <Bar data={data} options={categoryChartOptions}/>
+        </div>
+        <div className={`${styles.chartContainer} ${styles.paymentChartContainer}`}>
+          <h3 className={styles.chartTitle}>결제 방법</h3>
+          <Bar data={paymentMethodData} options={paymentMethodOptions} />
         </div>
       </div>
     </div>
